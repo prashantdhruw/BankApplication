972044db8abb017b5aaf9be26d53bd0925f2d33c
Prashant Dhruw <38435661+prashantdhruw@users.noreply.github.com>
2024-08-17 02:47:55 +0530

    Update BankxApplication.java

--- BankXapplication/src/main/java/com/bankx/BankxApplication.java
+++ BankXapplication/src/main/java/com/bankx/BankxApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankxApplication {
+public class BankXApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankxApplication.class, args);
